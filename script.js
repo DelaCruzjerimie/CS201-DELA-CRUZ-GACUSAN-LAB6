adba1ec9b9ce436388085bcfad965e96ac61fa1c
jerimie dela cruz <147166414+DelaCruzjerimie@users.noreply.github.com>
2023-10-10 22:39:08 +0800

    feat: added javascript into my html

---
+++ script.js
@@ -0,0 +1,9 @@
+// Print "Your Name" 100 times to the console using while loop
+// Iteration will start from 10
+// code with the while loop
+
+let i = 10;
+while (i <= 100) {
+  console.log("Jerimie R. Dela Cruz");
+  i++;
+}
